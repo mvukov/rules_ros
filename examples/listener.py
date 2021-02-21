b2026316f996124bb6b4206ea6bf422ad3c09014
Milan Vukov <mvukov@gmail.com>
2021-02-21 12:43:01 +0100

    Fix formatting for py-listener

--- examples/listener.py
+++ examples/listener.py
@@ -1,16 +1,16 @@
 import example_msgs.msg
 import rospy
 
+
 def callback(msg):
-  print(msg)
+    print(msg)
+
 
 def main():
-  rospy.init_node("listener", anonymous=True)
+    rospy.init_node("listener", anonymous=True)
+    sub = rospy.Subscriber("chatter", example_msgs.msg.Example, callback)
+    rospy.spin()
 
-  sub = rospy.Subscriber(
-      "chatter", example_msgs.msg.Example, callback)
-
-  rospy.spin()
 
 if __name__ == '__main__':
-	main()
+    main()
