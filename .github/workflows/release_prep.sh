380928c96ef27bd27df8c6510cf16e4a2f3e5312
Milan Vukov <mvukov@gmail.com>
2024-12-20 19:03:09 +0100

    Fix release_prep.sh

--- .github/workflows/release_prep.sh
+++ .github/workflows/release_prep.sh
@@ -2,9 +2,7 @@
 
 set -o errexit -o nounset -o pipefail
 
-# Set by GH actions, see
-# https://docs.github.com/en/actions/learn-github-actions/environment-variables#default-environment-variables
-TAG=${GITHUB_REF_NAME}
+TAG="$1"
 # The prefix is chosen to match what GitHub generates for source archives
 # This guarantees that users can easily switch from a released artifact to a source archive
 # with minimal differences in their code (e.g. strip_prefix remains the same)
